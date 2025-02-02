4206ce44c31007fdd920476c798cc92b0f35c16b
GiselleBarbosa <g88barbosa@gmail.com>
2025-02-02 19:04:17 -0300

    tests: adicionados testes do formulario

--- src/app/post-form/post-form.component.spec.ts
+++ src/app/post-form/post-form.component.spec.ts
@@ -1,20 +1,16 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PostFormComponent } from './post-form.component';
 
 describe('PostFormComponent', () => {
   let component: PostFormComponent;
   let fixture: ComponentFixture<PostFormComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ PostFormComponent ]
-    })
-    .compileComponents();
-  }));
+      imports: [PostFormComponent],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PostFormComponent);
@@ -24,5 +20,44 @@ describe('PostFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Deve preencher o input de titulo corretamente', () => {
+    const title = fixture.nativeElement.querySelector('input[name="title"]');
+    title.value = 'Titulo do post';
+    title.dispatchEvent(new Event('input'));
+
+    expect(component.form.controls.title.value).toBe('Titulo do post');
+  });
+
+  it('Deve preencher o textarea de corpo corretamente', () => {
+    const body = fixture.nativeElement.querySelector('textarea[name="body"]');
+    body.value = 'Corpo do post';
+    body.dispatchEvent(new Event('input'));
+
+    expect(component.form.controls.body.value).toBe('Corpo do post');
+  });
+
+  it('Deve enviar o formulario corretamente', () => {
+    spyOn(component.submitForm, 'emit');
+    //Simula o usuário digitando
+    const title = fixture.nativeElement.querySelector('input[name="title"]');
+    title.value = 'Titulo do post';
+    title.dispatchEvent(new Event('input'));
+
+    //Simula o usuário digitando
+    const body = fixture.nativeElement.querySelector('textarea[name="body"]');
+    body.value = 'Corpo do post';
+    body.dispatchEvent(new Event('input'));
+
+    // simula usuario submetendo o formulario
+    const form = fixture.nativeElement.querySelector('form');
+    form.dispatchEvent(new Event('submit'));
+
+    // Garante que o evento foi emitido corretamente
+    expect(component.submitForm.emit).toHaveBeenCalledWith({
+      title: 'Titulo do post',
+      body: 'Corpo do post',
+    });
   });
 });
