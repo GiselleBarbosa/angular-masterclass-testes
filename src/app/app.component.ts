e1c0607baea5a815fd02fb50e7c27eea5f6c9551
GiselleBarbosa <g88barbosa@gmail.com>
2025-02-02 18:24:39 -0300

    chore: remove conteudo do app component e importa component correspondente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { PostFormComponent } from "./post-form/post-form.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [PostFormComponent]
 })
 export class AppComponent {
   title = 'angular-masterclass-testes';
