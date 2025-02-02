a4aa84f6de3a45796418e7edefa841f95ef2796b
GiselleBarbosa <g88barbosa@gmail.com>
2025-02-02 19:43:07 -0300

    feat: adiciona signal de posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { PostFormComponent } from "./post-form/post-form.component";
+import { Post } from './models/post.model';
+import { PostCardComponent } from './post-card/post-card.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [PostFormComponent]
+  imports: [PostFormComponent, PostCardComponent],
 })
 export class AppComponent {
-  title = 'angular-masterclass-testes';
+  posts = signal<Post[]>([]);
+
+  addPost(post: Post) {
+    this.posts.update((posts) => [...posts, post]);
+  }
 }
