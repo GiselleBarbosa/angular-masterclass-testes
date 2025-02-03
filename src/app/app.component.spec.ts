94bc4b88dc41de5bb45b467d0bc8285c809ac24f
GiselleBarbosa <g88barbosa@gmail.com>
2025-02-03 07:42:21 -0300

    tests: implementa testes no app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('Deve adicionar um novo post a lista', () => {
+    component.addPost({ title: 'Titulo do post', body: 'Corpo do post' });
+    expect(component.posts()).toEqual([
+      { title: 'Titulo do post', body: 'Corpo do post' },
+    ]);
+
+    component.addPost({ title: 'Titulo do post 2', body: 'Corpo do post 2' });
+    expect(component.posts()).toEqual([
+      { title: 'Titulo do post', body: 'Corpo do post' },
+      { title: 'Titulo do post 2', body: 'Corpo do post 2' },
+    ]);
   });
 });
